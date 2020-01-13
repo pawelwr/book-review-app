7104c5ffb9756810a39ecd44213bd069ff3f1661
pawelwr <pawelwr1995@gmail.com>
2020-01-13 09:53:36 +0000

    query for create tables

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,32 @@
 from application import db
+from database import conn
 
-db.execute("""CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY
-    email VARCHAR NOT NULL
-    password VARCHAR NOT NULL
-    )""")
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    email VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL
+    );
+    
+CREATE TABLE book (
+    id serial PRIMARY KEY,
+    title VARCHAR,
+    author VARCHAR,
+    year INTEGER NOT NULL,
+    isbn INTEGER UNIQUE NOT NULL
+    );
+    
+CREATE TABLE review (
+    id serial PRIMARY KEY,
+    book_id INTEGER REFERENCES book(id),
+    user_id INTEGER REFERENCES users(id),
+    published TIMESTAMP,
+    content VARCHAR (500) NOT NULL
+    );
+    """)
+
+conn.commit()
+cursor.close()
+conn.close()

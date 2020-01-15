886eb9db38d21c839a555acfdb1c1ef128393cb3
pawelwr <pawelwr1995@gmail.com>
2020-01-15 15:40:26 +0000

    add route for book page

--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 import os
 import psycopg2
+import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from database import conn, gr_key
+from models import Book
 
 app = Flask(__name__)
 DATABASE_URL = "postgres://dyqjdqmwzfzjvw:1183c33c8bb8e2eb63c31da1f6496db492462e67c02d67ceec95a0e3caf25821@ec2-54-75-238-138.eu-west-1.compute.amazonaws.com:5432/dehg1it917u19a"
@@ -25,5 +29,21 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+    return render_template("layout.html")
 
-    return "Project 1: TODO"
+@app.route("/", methods=["POST"])
+def search():
+    text = request.form.get("search")
+    print(text)
+    result = Book.search(text)
+    print(result)
+    return render_template("search_result.html", text=text, result=result)
+
+@app.route("/book/<int:id>")
+def book_page(id):
+    book = Book.search_by_id(id)
+    gr_data = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": f"{gr_key}", "isbns": f"{id}"})
+    gr_json = gr_data.json()
+    avg_rank = gr_json['books'][0]['average_rating']
+    num_ratings = gr_json['books'][0]['work_ratings_count']
+    return render_template("book_page.html", avg_rank=avg_rank, num_ratings=num_ratings, book=book)

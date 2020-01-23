23a4dc7aea6c5017f7be0b95a2407f8dbc3d1f3c
pawelwr <pawelwr1995@gmail.com>
2020-01-23 12:37:32 +0000

    delete unused link

--- application.py
+++ application.py
@@ -15,7 +15,6 @@ from models import Book, User, Review
 from forms import LoginForm, RegisterForm
 
 app = Flask(__name__)
-DATABASE_URL = "postgres://dyqjdqmwzfzjvw:1183c33c8bb8e2eb63c31da1f6496db492462e67c02d67ceec95a0e3caf25821@ec2-54-75-238-138.eu-west-1.compute.amazonaws.com:5432/dehg1it917u19a"
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):

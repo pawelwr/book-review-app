06b5232a813b5ece0950828b518bbc41c38af928
pawelwr <pawelwr1995@gmail.com>
2020-01-23 10:33:38 +0000

    do password confirm

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms.fields.html5 import EmailField
 
 class LoginForm(FlaskForm):
@@ -11,6 +11,8 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), 
+                EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat password')
     email = EmailField('Email address', validators=[DataRequired(), Email()])
     submit = SubmitField('Sign up')
